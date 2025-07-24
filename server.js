b9e0d7407ab0107994227f83bd1bf3e431ab5769
Rudrasish <rudrasish2003@gmail.com>
2025-07-24 14:43:28 +0530

    Changed express middleware

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+
 
 // ✅ Allowed voice IDs
 const allowedVoiceIds = [
